631e5179d0d1bf319d13c1f3f3152543a1bcd340
fefernando789729@gmail.com <fernando789729@gmail.com>
2026-03-24 10:12:17 -0700

    Cambios en el archivo index

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 
+// Middleware para poder recibir JSON en las peticiones (buena práctica)
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API funcionando');
+});
+
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Fernando Ochoa',
+    rol: 'Administrador',
+    activo: true,
+    fechaCreacion: '2025-03-24'
+  };
+
+  res.json(usuario);
 });
 
 app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+  console.log('🚀 Servidor corriendo en http://localhost:3000');
 });
