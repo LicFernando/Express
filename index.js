fa2b226b77c2a1ecb24e9c963f9bbe76248e1388
fefernando789729@gmail.com <fernando789729@gmail.com>
2026-03-25 10:10:13 -0700

    Integración de PostgreSQL - conexión con pool

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const express = require('express');
+const pool = require('./db');
 const app = express();
-
-// Middleware para poder recibir JSON en las peticiones (buena práctica)
-app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
-app.get('/usuario', (req, res) => {
-  const usuario = {
-    id: 1,
-    nombre: 'Fernando Ochoa',
-    rol: 'Administrador',
-    activo: true,
-    fechaCreacion: '2025-03-24'
-  };
-
-  res.json(usuario);
-});
+pool.connect()
+  .then(() => {
+    console.log('Conexión exitosa a PostgreSQL');
+  })
+  .catch((err) => {
+    console.error('Error de conexión', err);
+  });
 
 app.listen(3000, () => {
-  console.log('🚀 Servidor corriendo en http://localhost:3000');
+  console.log('Servidor corriendo en http://localhost:3000');
 });
